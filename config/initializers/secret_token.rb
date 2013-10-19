38ee6b04d3b127f2ef3fc7917a9babdeab9aace1
ivandimitrov07 <idimitrov07@yahoo.com>
2013-10-19 23:55:53 +0300

    initial commit, created rails app, added devise, simple_form, thin to gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pinapp::Application.config.secret_token = 'a8bab518707a77448c9e118d36c7c9b0d1eb06c5e25a62c7be27f214196388706c1e3ddbea053f61d39ef4533c1b2c3ddf9af8b1a52dffcd9badfe7f7eb519d0'
