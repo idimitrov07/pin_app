9e6470df12650b5aa53509f9dd67c9b8125cac32
ivandimitrov07 <idimitrov07@yahoo.com>
2013-10-20 12:23:02 +0300

    added devise

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Pinapp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #devise recommendation
+    config.assets.initialize_on_precompile = false
   end
 end
